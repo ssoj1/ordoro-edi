530267cb172ab5b2c0194382aa175b56a77d4998
SARA JENSEN <SSOJENSEN@GMAIL.COM>
2022-01-17 10:55:05 -0800

    adding check_unique_email and check_domain functions as well as loop. check_april_login is WIP

--- app.py
+++ app.py
@@ -1,11 +1,52 @@
 import requests
+from datetime import datetime
 
 response = requests.get(
     "https://us-central1-marcy-playground.cloudfunctions.net/ordoroCodingTest")
 
+respose_data = response.json()['data']
+
 unique_emails = {} #set - needs to be unique
-user_domain_count = {}
+user_domain_counts = {}
 april_emails = []
+
+def check_unique_email(email):
+    """Accepts a string email address and checks if it's in unique_emails. 
+    If not, adds the email. 
+    Returns None. 
+    """
+    if email not in unique_emails:
+        unique_emails.add(email)
+
+
+def check_domain(email):
+    """Accepts a string email. If email domain is in user_domain_counts, 
+    increases count by 1.
+    If not, adds the domain and sets count to 1.
+    Returns None. 
+    """
+    domain = email.split('@')[1]
+    user_domain_counts[domain]= user_domain_counts.get(domain, 0) + 1
+
+# def check_april_login(timestamp):
+#     """ Accepts a timestamp from the API formatted as TO DO!!!!!!!!!!!"""
+#     utc_datetime = datetime.utcfromtimestamp(timestamp)
+#     print(utc_datetime)
+
+# check_april_login('2014-04-22T09:31:56+04:00')
+
+
+for data in respose_data:
+    # add to unique_emails if not already there
+    check_unique_email(data['email'])
+    
+    # add domain to user_domain_counts and increase count
+    check_domain(data['email'])
+
+    # add email to april_emails if user logged in in April
+
+
+
 
 # TO DO:
 #   response.status_code - for a try/ except?
@@ -23,3 +64,26 @@ april_emails = []
 #           "april_emails": ["email1@test.com", "email2@test.com"]
 #       }
 #   runtime complexity
+
+
+# DECISION NOTES:
+    # DECISION TO USE REQUESTS
+    # did a bit of research and decided to use requests library - not part of the 
+    # standard library, so installed and imported
+
+    # API DOCS SAY THE RESPONSE IS JSON
+    # so using the standard library json package
+
+    # Classes or helper functions?
+    # Testability 
+
+    # To run tests: 
+        # python3 -m doctest -v app.py
+
+
+# Time
+    # Thinking about it after reading
+        # Googled a couple of random things
+
+
+#c
